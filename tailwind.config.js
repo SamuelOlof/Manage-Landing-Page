a8a4ef160462bb257afea32a946ca0049ac894f3
SamuelOlof <samuelolofintoye@gmail.com>
2021-03-04 19:26:11 +0300

    Manage landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         'dark-grayish-blue' : 'hsl(227, 12%, 61%)',
         'very-dark-blue' : 'hsl(233, 12%, 13%)',
         'very-pale-red' : 'hsl(13, 100%, 96%)',
-        'vary-light-gray' : 'hsl(0, 0%, 98%)',
+        'very-light-gray' : 'hsl(0, 0%, 98%)',
       }
     },
   },
